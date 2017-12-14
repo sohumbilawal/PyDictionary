b358ef6982373b2b60450f0411d6b58c631e1adc
sohumbilawal <sohumbilawal@gmail.com>
2017-12-14 12:46:58 -0800

    A little more sophisticated with better intelligence.
    Added if statements to take care of multiple scenarios that a user might encounter and logic to take care of each. Last step is to make the output not a list, but a 'one meaning per line' clean output.

--- pyDict1.py
+++ pyDict1.py
@@ -7,17 +7,23 @@ def wordDef(inpKey):
     from difflib import get_close_matches
 
     if inpKey in dictCon:
-        return dictCon[inpKey]
+
+        return dictCon[inpKey] #This produces an output.
+
     elif len(get_close_matches(inpKey, dictCon.keys(), n=1, cutoff=0.8)):
 
         probWord = get_close_matches(inpKey, dictCon.keys(), n=1, cutoff=0.8)[0]
 
         ans = input("Did you mean {}? If Yes, type Y, if No, type N: ".format(probWord))
 
-        if ans == "Y" or ans == "y":
+        ans = ans.lower()
+
+        if ans == "y":
             return dictCon[probWord]
-        else:
+        elif ans == "n":
             return "Sorry your word could not be found!"
+        else:
+            return "We did not understand your query."
     else:
 
         return "Sorry your word could not be found!"
